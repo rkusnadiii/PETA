5b0de4ee5ed592b33431929050c74ec95b4567c7
rkusnadiii <51116850+rkusnadiii@users.noreply.github.com>
2021-10-04 14:01:47 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.myapplication.databinding.ActivityRegisterBinding;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+}
