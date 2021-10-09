e216982dafd9a26b3e6686da1dcf9c664b18a538
diofajrie <diofajriefadlullah17@gmail.com>
2021-10-09 16:54:35 +0700

    versi doi

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -7,6 +7,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,8 +17,22 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.databinding.ActivityRegisterBinding;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
-
+    EditText email;
+    EditText password;
+    Button register;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseApp.initializeApp(this);
+        email = findViewById(R.id.edtEmailRegister);
+        password = findViewById(R.id.edtPasswordRegister);
+        register = findViewById(R.id.btnRegisterRegister);
+    }
 }
