f698f0bdf68a8f0499c6a0b2751525be1a464068
diofajrie <diofajriefadlullah17@gmail.com>
2021-10-09 11:42:09 +0700

    versi doi

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,32 @@
 package com.example.myapplication;
 
-public class MainActivity{
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText emailLogin, passwordLogin;
+    Button Login, Register;
+    FirebaseAuth firebaseAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        emailLogin = findViewById(R.id.edtEmailLogin);
+        passwordLogin = findViewById(R.id.edtPasswordLogin);
+        firebaseAuth = firebaseAuth.getInstance();
+        Register = findViewById(R.id.btnRegisterLogin);
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        Login = findViewById(R.id.btnLoginLogin);
+    }
 }
