7e5f19c705c746d4d725d635d9d35c5f367dc4ae
diofajrie <diofajriefadlullah17@gmail.com>
2021-12-02 16:49:43 +0700

    xml fragmen home&profile

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/FragmentHomeBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/FragmentHomeBinding.java
@@ -4,31 +4,25 @@ package com.example.myapplication.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
 import com.example.myapplication.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
-import java.lang.String;
 
 public final class FragmentHomeBinding implements ViewBinding {
   @NonNull
-  private final RelativeLayout rootView;
+  private final LinearLayout rootView;
 
-  @NonNull
-  public final TextView textHome;
-
-  private FragmentHomeBinding(@NonNull RelativeLayout rootView, @NonNull TextView textHome) {
+  private FragmentHomeBinding(@NonNull LinearLayout rootView) {
     this.rootView = rootView;
-    this.textHome = textHome;
   }
 
   @Override
   @NonNull
-  public RelativeLayout getRoot() {
+  public LinearLayout getRoot() {
     return rootView;
   }
 
@@ -49,19 +43,10 @@ public final class FragmentHomeBinding implements ViewBinding {
 
   @NonNull
   public static FragmentHomeBinding bind(@NonNull View rootView) {
-    // The body of this method is generated in a way you would not otherwise write.
-    // This is done to optimize the compiled bytecode for size and performance.
-    int id;
-    missingId: {
-      id = R.id.textHome;
-      TextView textHome = rootView.findViewById(id);
-      if (textHome == null) {
-        break missingId;
-      }
-
-      return new FragmentHomeBinding((RelativeLayout) rootView, textHome);
+    if (rootView == null) {
+      throw new NullPointerException("rootView");
     }
-    String missingId = rootView.getResources().getResourceName(id);
-    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
+
+    return new FragmentHomeBinding((LinearLayout) rootView);
   }
 }
--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/FragmentProfileBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/myapplication/databinding/FragmentProfileBinding.java
@@ -4,31 +4,25 @@ package com.example.myapplication.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
 import com.example.myapplication.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
-import java.lang.String;
 
 public final class FragmentProfileBinding implements ViewBinding {
   @NonNull
-  private final RelativeLayout rootView;
+  private final LinearLayout rootView;
 
-  @NonNull
-  public final TextView textProfile;
-
-  private FragmentProfileBinding(@NonNull RelativeLayout rootView, @NonNull TextView textProfile) {
+  private FragmentProfileBinding(@NonNull LinearLayout rootView) {
     this.rootView = rootView;
-    this.textProfile = textProfile;
   }
 
   @Override
   @NonNull
-  public RelativeLayout getRoot() {
+  public LinearLayout getRoot() {
     return rootView;
   }
 
@@ -49,19 +43,10 @@ public final class FragmentProfileBinding implements ViewBinding {
 
   @NonNull
   public static FragmentProfileBinding bind(@NonNull View rootView) {
-    // The body of this method is generated in a way you would not otherwise write.
-    // This is done to optimize the compiled bytecode for size and performance.
-    int id;
-    missingId: {
-      id = R.id.textProfile;
-      TextView textProfile = rootView.findViewById(id);
-      if (textProfile == null) {
-        break missingId;
-      }
-
-      return new FragmentProfileBinding((RelativeLayout) rootView, textProfile);
+    if (rootView == null) {
+      throw new NullPointerException("rootView");
     }
-    String missingId = rootView.getResources().getResourceName(id);
-    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
+
+    return new FragmentProfileBinding((LinearLayout) rootView);
   }
 }
